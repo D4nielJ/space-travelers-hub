af249914b021380d0028e71fe035478fbdd4c611
d4nielj <d4niel.djm@gmail.com>
2021-08-31 10:31:05 -0500

    Add missions joined to profile

--- src/features/profile/profile.js
+++ src/features/profile/profile.js
@@ -1,3 +1,26 @@
-const Profile = () => <div>This is the profile</div>;
+import { useSelector } from 'react-redux';
+
+const Profile = () => {
+  const missions = useSelector((state) => state.missions.missions);
+  const createMissions = () =>
+    missions.map((mission) => {
+      if (mission.status) {
+        return <li key={mission.missionId}>{mission.name}</li>;
+      }
+    });
+  
+  return (
+    <section className="profile">
+      <div className="missions">
+        <h2>My Missions</h2>
+        <ul>{createMissions()}</ul>
+      </div>
+      <div className="rockets">
+        <h2>My Rockets</h2>
+        <ul>{/* {createRockets()} */}</ul>
+      </div>
+    </section>
+  );
+};
 
 export default Profile;
