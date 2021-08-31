0de5fd9c7b55be302743237444493d4ef56d7d75
d4nielj <d4niel.djm@gmail.com>
2021-08-31 10:12:42 -0500

    Add update mission status actions

--- src/features/missions/missions.js
+++ src/features/missions/missions.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissions } from './missionsSlice';
+import { fetchMissions, updateMissions } from './missionsSlice';
 
 const Missions = () => {
   const dispatch = useDispatch();
@@ -13,20 +13,30 @@ const Missions = () => {
     }
   }, []);
 
-  const createMissions = () => missions.map((mission) => (
-    <tr key={mission.missionId}>
-      <th>{mission.name}</th>
-      <th>{mission.description}</th>
-      <th>{mission.status ? 'Active member' : 'Not a member'}</th>
-      <th>
-        {mission.status ? (
-          <button type="button">Leave mission</button>
-        ) : (
-          <button type="button">Join mission</button>
-        )}
-      </th>
-    </tr>
-  ));
+  const handleUpdateMissionBtn = (e) => {
+    const id = e.target.parentNode.parentNode.id;
+    dispatch(updateMissions(id));
+  };
+
+  const createMissions = () =>
+    missions.map((mission) => (
+      <tr id={mission.missionId} key={mission.missionId}>
+        <th>{mission.name}</th>
+        <th>{mission.description}</th>
+        <th>{mission.status ? 'Active member' : 'Not a member'}</th>
+        <th>
+          {mission.status ? (
+            <button onClick={handleUpdateMissionBtn} type="button">
+              Leave mission
+            </button>
+          ) : (
+            <button onClick={handleUpdateMissionBtn} type="button">
+              Join mission
+            </button>
+          )}
+        </th>
+      </tr>
+    ));
 
   return (
     <table>
